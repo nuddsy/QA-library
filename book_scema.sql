bc892fad921628e22c818b5093598c67eb389c64
Matt Nudds <matthewnudds1@gmail.com>
2024-10-25 12:17:03 +0100

    ADD: author and book are linked (PK/FK)

--- book_scema.sql
+++ book_scema.sql
@@ -2,6 +2,10 @@ create table book(
 	book_id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(50) NOT NULL,
     genre_id INT NOT NULL,
+    author_id INT NOT NULL,
     
-    FOREIGN KEY (genre_id) REFERENCES genre(genre_id)
+    FOREIGN KEY (genre_id) REFERENCES genre(genre_id),
+    FOREIGN KEY (author_id) REFERENCES author(author_id)
 );
+
+DROP TABLE book;

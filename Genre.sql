ed94fc274a00e569598b5040cfd5bdd42e210512
NevaDobric <neva.dobric@gmail.com>
2024-10-25 14:41:09 +0100

    EDIT typos corrected

--- Genre.sql
+++ Genre.sql
@@ -1,11 +1,11 @@
 use library_db;
 
-CREATE TABLE Genre (
+CREATE TABLE genre (
 	genre_id INT PRIMARY KEY AUTO_INCREMENT,
     genre VARCHAR(50) NOT NULL
 );
 
-SELECT * FROM Genre;
+SELECT * FROM genre;
 
 INSERT INTO genre(genre)
 VALUES("Comedy"),
